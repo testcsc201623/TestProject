769df8d1e70f1c5be7eab8a82b386a3179d67a78
testcsc201623 <suehiro@jcsc.co.jp>
2023-10-16 10:37:27 +0900

    削除ボタンのポップアップ実装

--- src/main/resources/static/js/accountList.js
+++ src/main/resources/static/js/accountList.js
@@ -1,9 +1,10 @@
-$('#deleteAlert').on('show.bs.modal', function (event) {
-  let button = $(event.relatedTarget); //モーダルを呼び出すときに使われたボタンを取得
-  let recipient = button.data('whatever'); //data-whatever の値を取得
+window.addEventListener('DOMContentLoaded', function(){
+  /** jQueryの処理 */
+  $('#deleteAlert').on('show.bs.modal', function(event) {
+		let button = $(event.relatedTarget); //モーダルを呼び出すときに使われたボタンを取得
+		let recipient = button.data('whatever'); //data-whatever の値を取得
 
-  //Ajaxの処理はここに
-
-  let modal = $(this);  //モーダルを取得
-  modal.find('.modal-footer input#deleteUserId').val(recipient); //inputタグにも表示
-})
+		let modal = $(this);  //モーダルを取得
+		modal.find('.modal-footer input#deleteUserId').val(recipient); //inputタグにも表示
+	});
+});
